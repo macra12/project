0cf2e860a7f8f24eca000149d20b0fb7cbd22983
macra12 <khinnara99@gmail.com>
2024-12-01 14:15:53 +0700

    Add or update missing files

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('Service Worker registered with scope:', registration.scope);
+  navigator.serviceWorker.register('/service-worker.js')
+      .then(function(registration) {
+          console.log('Service Worker registered with scope:', registration.scope);
       })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
+      .catch(function(error) {
+          console.log('Service Worker registration failed:', error);
       });
-  });
 }

97d141a9133160c478e2ac9834b00cce3172a502
macra12 <khinnara99@gmail.com>
2024-12-01 13:39:25 +0700

    Update files for offline support

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,24 @@
 const CACHE_NAME = 'offline-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
 ];
 
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('Caching files');
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching files');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
